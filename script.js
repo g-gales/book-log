11109ea5c863fc68ef7e48cd34f97422ffe21fb2
Grayson Gales <graysongales15@gmail.com>
2025-02-25 01:31:18 -0500

    reorganized js file

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ import { initializeApp } from "firebase/app";
 import {
   doc,
   getDocs,
+  getDoc,
   addDoc,
   updateDoc,
   getFirestore,
@@ -52,7 +53,7 @@ const db = getFirestore(app);
 
 //Add data to Firebase
 async function addBookToFirestore(bookTitle, bookAuthor, bookGenre) {
-  await addDoc(collection(db, "books"), {
+  let book = await addDoc(collection(db, "books"), {
     genre: bookGenre,
     author: bookAuthor,
     title: bookTitle,
@@ -60,6 +61,7 @@ async function addBookToFirestore(bookTitle, bookAuthor, bookGenre) {
     rating: 0,
     removed: false,
   });
+  return book.id;
 }
 
 //Fetch books from Firestore when app loads
@@ -72,67 +74,70 @@ async function getBooksFromFirestore() {
   return userData;
 }
 
+//Chatbot
+function ruleChatBot(request) {
+  if (request.startsWith("add book")) {
+    let book = request.replace("add book", "").trim();
+    if (book) {
+      addBook(task);
+      appendMessage("Task " + task + " added!");
+    } else {
+      appendMessage("Please specify a task to add.");
+    }
+    return true;
+  } else if (request.startsWith("complete")) {
+    let taskName = request.replace("complete", "").trim();
+    if (taskName) {
+      if (removeFromTaskName(taskName)) {
+        appendMessage("Task " + taskName + " marked as complete.");
+      } else {
+        appendMessage("Task not found!");
+      }
+    } else {
+      appendMessage("Please specify a task to complete.");
+    }
+    return true;
+  }
+
+  return false;
+}
+
 //Render books
 async function renderBooks() {
   var books = await getBooksFromFirestore();
   bookList.innerHTML = "";
 
+  let bookArr = [];
+
   books.forEach((book) => {
     const bookData = book.data();
 
-    if (!bookData.removed) {
-      const bookItem = document.createElement("article");
-      bookItem.id = book.id;
-      bookItem.tabIndex = 0;
-      bookItem.classList.add("book");
-      //Adds a new book item, if the book is yet rated, adds an input to rate it
-      bookItem.innerHTML = `
-        <div class="book-header">
-          <h3 class="book-title">${bookData.title}</h3>
-          <p class="book-author">By: ${bookData.author}</p>
-        </div>
-        <p class="book-genre"> Genre: ${
-          genreMap[bookData.genre] || "Unknown"
-        }</p>
-        <div class="book-rating"> ${
-          bookData.read
-            ? `<p class="read">Rating: ${bookData.rating}/5</p>`
-            : `<p class="not-read">Submit a Rating When Read</p>
-            <div class="book-rating-input">
-              <input type="number" min="1" max="5" placeholder="1-5" id="rate-${book.id}" />
-              <button id="submit-${book.id}">I've Read It!</button>
-            </div>`
-        }
-        </div>
-        <button id="remove-${book.id}" class="remove-btn">Remove Book</button>`;
+    bookArr.push({
+      id: book.id,
+      author: bookData.author,
+      genre: bookData.genre,
+      rating: bookData.rating,
+      read: bookData.read,
+      title: bookData.title,
+      removed: bookData.removed,
+    });
+  });
 
-      bookList.appendChild(bookItem);
+  bookArr.sort((a, b) => {
+    const authorA = a.author.toUpperCase();
+    const authorB = b.author.toUpperCase();
+    if (authorA < authorB) {
+      return -1;
+    }
+    if (authorA > authorB) {
+      return 1;
+    }
+    return 0;
+  });
 
-      //adds functionality to new rating button
-      if (!bookData.read) {
-        const submitButton = document.getElementById(`submit-${book.id}`);
-        submitButton.addEventListener("click", async () => {
-          const ratingInput = document.getElementById(`rate-${book.id}`);
-          const ratingValue = parseInt(ratingInput.value);
-
-          if (ratingValue >= 1 && ratingValue <= 5) {
-            await updateBookRating(book.id, ratingValue);
-            renderBooks();
-          } else {
-            alert("Please enter a valid rating between 1 and 5.");
-          }
-        });
-      }
-
-      const removeButton = document.getElementById(`remove-${book.id}`);
-      removeButton.addEventListener("click", async (e) => {
-        if (e.target.parentNode.tagName === "ARTICLE") {
-          await updateDoc(doc(db, "books", e.target.parentNode.id), {
-            removed: true,
-          });
-        }
-        renderBooks();
-      });
+  bookArr.forEach((book) => {
+    if (!book.removed) {
+      createBookItem(book.id, book);
     }
   });
 }
@@ -147,6 +152,87 @@ async function updateBookRating(bookId, rating) {
 }
 
 //Add book
+async function addBook(bookTitle, bookAuthor, bookGenre) {
+  let book = await addBookToFirestore(bookTitle, bookAuthor, bookGenre);
+  bookAuthorInput.value = "";
+  bookGenreInput.value = "";
+  bookTitleInput.value = "";
+
+  let bookObject = await getDoc(book);
+  createBookItem(book.id, bookObject.data());
+}
+
+//Remove book from firestore rendering
+async function removeBook(bookId) {
+  await updateDoc(doc(db, "books", bookId), {
+    removed: true,
+  });
+}
+
+//Remove book from DOM
+function removeVisualBook(bookId) {
+  document.getElementById(bookId).remove();
+}
+
+//Create Book DOM Element
+function createBookItem(bookId, bookObject) {
+  let bookItem = document.createElement("article");
+
+  bookItem.id = bookId;
+  bookItem.tabIndex = 0;
+  bookItem.classList.add("book");
+  //Adds a new book item, if the book is yet rated, adds an input to rate it
+  bookItem.innerHTML = `
+    <div class="book-header">
+      <h3 class="book-title">${bookObject.title}</h3>
+      <p class="book-author">By: ${bookObject.author}</p>
+    </div>
+    <p class="book-genre"> Genre: ${genreMap[bookObject.genre] || "Unknown"}</p>
+    <div class="book-rating"> ${
+      bookObject.read
+        ? `<p class="read">Rating: ${bookObject.rating}/5</p>`
+        : `<p class="not-read">Submit a Rating When Read</p>
+        <div class="book-rating-input">
+          <input type="number" min="1" max="5" placeholder="1-5" id="rate-${bookId}" />
+          <button id="submit-${bookId}">I've Read It!</button>
+        </div>`
+    }
+    </div>
+    <button id="remove-${bookId}" class="remove-btn">Remove Book</button>`;
+
+  bookList.appendChild(bookItem);
+
+  //  adds functionality to new rating button
+  if (!bookObject.read) {
+    const submitButton = document.getElementById(`submit-${bookId}`);
+    submitButton.addEventListener("click", async () => {
+      const ratingInput = document.getElementById(`rate-${bookId}`);
+      const ratingValue = parseInt(ratingInput.value);
+
+      if (ratingValue >= 1 && ratingValue <= 5) {
+        await updateBookRating(bookId, ratingValue);
+        renderBooks();
+      } else {
+        alert("Please enter a valid rating between 1 and 5.");
+      }
+    });
+  }
+
+  const removeButton = document.getElementById(`remove-${bookId}`);
+  removeButton.addEventListener("click", async (e) => {
+    if (e.target.parentNode.tagName === "ARTICLE") {
+      removeBook(e.target.parentNode.id);
+      removeVisualBook(e.target.parentNode.id);
+    }
+  });
+}
+
+// Add books on page load
+window.addEventListener("load", () => {
+  getApiKey();
+  renderBooks();
+});
+
 addBookBtn.addEventListener("click", async (event) => {
   event.preventDefault(); //Prevent default form submission
 
@@ -159,19 +245,10 @@ addBookBtn.addEventListener("click", async (event) => {
     const bookAuthor = sanitizeInput(author);
     const bookGenre = sanitizeInput(genre);
 
-    await addBookToFirestore(bookTitle, bookAuthor, bookGenre);
-    bookTitleInput.value = "";
-    bookAuthorInput.value = "";
-    bookGenreInput.value = "";
-    renderBooks();
+    await addBook(bookTitle, bookAuthor, bookGenre);
   } else {
     alert("Please fill out all fields!");
   }
-});
-
-// Add books on page load
-window.addEventListener("load", () => {
-  renderBooks();
 });
 
 //Sanitize Data
@@ -180,6 +257,31 @@ function sanitizeInput(input) {
   div.textContent = input;
   return div.innerHTML;
 }
+
+bookAuthorInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    addBookBtn.click();
+  }
+});
+
+bookTitleInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    addBookBtn.click();
+  }
+});
+
+bookGenreInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    addBookBtn.click();
+  }
+});
+
+bookList.addEventListener("keypress", async function (e) {
+  if (e.target.className === "remove-btn" && e.key === "Enter") {
+    removeBook(e.target.id);
+    removeVisualBook(e.target.id);
+  }
+});
 
 //Error Handling and Logging
 window.addEventListener("error", function (event) {
